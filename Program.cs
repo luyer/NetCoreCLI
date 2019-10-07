a2ae4d2bec32d3ca07440e564555559fb4ab1b30
luis.guerra <luis.guerra@neoris.com>
2019-10-07 16:09:03 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello i name is LUIS!");
+        }
+    }
+}
